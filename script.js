f9016d2ef90dbeba3a9ef0b2c8094d5e6480879f
Trueax <jrt5617@psu.edu>
2022-10-18 20:40:22 -0400

    added jquery code from oct 18 session

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-document.getElementById('demo').innerHTML = 'new text';
-document.getElementById('demo').id = "demo2";
-document.getElementById('demo2').style.color = "#00FF00";
-document.getElementById('demo2').outerHTML = "<button>something to press</button>";
-
-function myFun() {
-    alert("WARNING ALERT OMG!!!!");
+$(document).ready(function() {
+    $("#btn1").mouseup(function() {
+        // alert("alert");
+        // $("#div1").slideToggle(3000);
+        let div = $("div");
+        div.animate({left:"400px", height:"300px", opacity:".5"},"fast");
+    }
+    );
 }
-myFun();
+);
 
-function darkMode() {
-    document.getElementById("pagestyle").setAttribute("href", "./style-dark.css")
-}
 
-var a = 10;
-let b = 20;
-const c = 30;
+// document.getElementById('demo').innerHTML = 'new text';
+// // document.getElementById('demo').id = "demo2";
+// document.getElementById('demo').style.color = "#00FF00";
+// // document.getElementById('demo').outerHTML = "<button>something to press</button>";
 
-a += 10;
-b += 10;
+// function myFun() {
+//     alert("WARNING ALERT OMG!!!!");
+// }
+// // myFun();
 
-console.log(a+b+c);
+// function darkMode() {
+//     document.getElementById("pagestyle").setAttribute("href", "./style-dark.css")
+// }
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// a += 10;
+// b += 10;
+
+// console.log(a+b+c);
+
+// function key_pressed(input) {
+//     console.log(input);
+//     document.getElementById("keypressed_text").innerHTML = input;
+// }
+
+// const key = function keyEvent(e) {
+//     key_pressed(e.key);
+//     if (e.key === "f") {
+//         alert("f key pressed");
+//     }
+// }
+
+// document.addEventListener("keypress", key);
